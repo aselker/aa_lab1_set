f7c1fb305e5e7bc000b9f953f917956b356b9f96
NickShermeister <ncsherman159@gmail.com>
2020-03-22 13:39:28 -0400

    pushing read_input

---
+++ read_input.py
@@ -0,0 +1,39 @@
+import random
+
+
+def read_input(file):
+    # Step 1: Read input
+    # Using readlines()
+    file1 = open(file, 'r')
+    lines = file1.readlines()
+
+    count = 0
+    # Strips the newline character
+    cleaned_input = []
+    num_dims = None
+    num_vars = None
+    for line in lines:
+        stripped = line.strip()
+        if stripped[0] == "c":
+            pass
+        elif stripped[0] == "p":
+            split = stripped.split()
+            num_dims = split[1]
+            num_vars = split[2]
+        else:
+            cleaned_input.append([int(x) for x in stripped.split()])
+
+    num_cards = len(cleaned_input)
+    print(cleaned_input)
+    print(f'num_vars is: {num_vars}')
+    print(f'num_dims is: {num_dims}')
+    return (cleaned_input, num_cards, num_dims, num_vars)
+
+
+
+if __name__ == "__main__":
+    files = ["test_3dmapping.txt"]
+    for file in files:
+        print(f'File name: {file}')
+        set_out = read_input(file)
+        print(set_out)
