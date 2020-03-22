5b68a4bb89ba8d387779fb86e727ed65ad25c6b2
Adam Selker <adamselker@gmail.com>
2020-03-22 10:42:42 -0700

    Random board maker

--- set_solver.py
+++ set_solver.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+
+import itertools
+import random
+
+"""
+A solver for the card game Set, that finds all valid sets.  Works with
+arbitrary numbers of properties and values.
+
+Cards are represented as tuples of k values, each from 0 to v-1, where k is the
+number of properties, and v the number of values.  In a normal game, k=4
+(shape, color, number, infigg) and v=3 (oval, diamond, squiggle; solid, lines,
+empty; etc.).  A set is a list of cards.
+"""
+
+
+def random_board(k, v, n):
+    """
+    Generate a random Set board with k=k, v=v, and n cards.
+    """
+    deck = list(itertools.product(*[list(range(v)) for _ in range(k)]))
+    return random.sample(deck, n)
+
+
+if __name__ == "__main__":
+    print(random_board(4, 3, 12))
